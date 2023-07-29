26816c214cd092fee94ac6d7ba9f835887e54fe7
camcimber <camryn.cimber@ufl.edu>
2023-07-29 13:30:15 -0400

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div 
+    name="about" 
+    className="w-full h-screen bg-gradient-to-b from-green-800 to-black text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                    About
+                </p>
+            </div>
+
+            <p className="text-xl py-6">
+            Hi! My name is Camryn Cimber, and I am a student with a passion for technology. I enjoy solving complex problems and collaborating in teams to find the most optimal solutions. Being a lifelong learner, I am constantly exploring different fields and concepts in the tech world to discover my true passion.            </p>
+
+            <br />
+
+            <p className="text-xl">
+            My primary interests lie in software engineering, UI/UX design, and full-stack development. I find joy in working with all layers of the stack and creating user-friendly, efficient websites.            
+            </p>
+
+            <br />
+            <p className="text-xl">
+            My journey into tech began in high school when I took my first computer science course. Although I felt intimidated and lacked confidence in my skills, I persevered with the help of guidance and mentors. In college, I continued to explore different areas within technology, and my interest in creating web applications grew.
+            </p>
+            <br />
+            <p className="text-xl">
+            As I continue to gain experiences, I am actively seeking a company that shares my values of a passion for learning and providing a positive working environment. I am excited about the possibilities that lie ahead and look forward to contributing my skills and knowledge to make a difference in the tech industry.
+            </p>
+        </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import bonapp from '../assets/portfolio/bonapp.png'
+import nf from '../assets/portfolio/NF.png'
+import installNode from '../assets/portfolio/installNode.jpg'
+import stay from '../assets/portfolio/stayOnTask.png'
+import sweeper from '../assets/portfolio/sweeper.png'
+import stick from '../assets/portfolio/stick.png'
+import reactWeather from '../assets/portfolio/reactWeather.jpg'
+
+const Portfolio = () => {
+
+  const portfolios = [
+    {
+      id: 1,
+      src: bonapp,
+      href: 'https://github.com/camcimber/Recipe-App',
+    },
+    {
+      id: 2,
+      src: nf,
+      href: 'https://github.com/camcimber/Nutritional-Facts',
+    },
+    {
+      id: 3,
+      src: stay,
+      href: 'https://github.com/camcimber/Stay-on-Task',
+    },
+    {
+      id: 4,
+      src: sweeper,
+      href: 'https://github.com/camcimber/Minesweeper',
+    },
+    {
+      id: 5,
+      src: stick,
+      href: 'https://www.dropbox.com/scl/fi/xiwaw7kp3s23i6jav2n6p/annotated-Final-20Design-20Report-20-282-29.docx.pdf?rlkey=q0ctfuzdvhj9w43qh32i1k7un&dl=0',
+    },
+    // {
+    //   id: 6,
+    //   src: reactWeather,
+    //   href: 'https://linkedin.com/in/camryn-cimber',
+    // }
+  ];
+
+  const handleCodeButtonClick = (link) => {
+    window.open(link, '_blank'); // Open the link in a new tab
+  };
+
+  return <div
+          name="portfolio" 
+          className='bg-gradient-to-b from-black to-green-800 w-full text-white md:h-screen'>
+
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+          <div className="pb-8">
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+              Portfolio</p>
+            <p className='py-6'>Check out some of my work right here: </p>
+          </div>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+
+          {portfolios.map(({id, src, href}) =>(
+           <div key={id} className="shadow-md shadow-black rounded-lg">
+                <img 
+                    src={src} 
+                    alt="" 
+                    className="rounded-md duration-200 hover:scale-105"
+                 />
+                <div className="flex items-center justify-center">
+                    {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button> */}
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => handleCodeButtonClick(href)}>Learn More</button>
+               </div>
+              <a href={href}></a>
+           </div>
+          ))}
+
+           </div>
+       </div> 
+    </div>
+  
+};
+
+export default Portfolio;
